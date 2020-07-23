20700ffbaab4e8fd121b054d3bf728475a0fd7ed
Ilham Rahman <ilhaamrahman@gmail.com>
2020-07-23 11:56:44 +0700

    add google map

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import "./App.scss";
+import Homepage from "./Homepage";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Marker.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Marker = () => {
+  return (
+    <div
+      style={{
+        height: "10px",
+        width: "10px",
+        backgroundColor: "#000000",
+        borderRadius: "100px",
+      }}
+    ></div>
+  );
+};
+
+export default Marker;
